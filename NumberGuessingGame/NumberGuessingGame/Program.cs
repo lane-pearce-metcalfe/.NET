30d7e50b64ae7195c130db1076d5d6e886f67fe4
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-08-12 15:35:58 +1200

    Created a loop which allows the user to exit or stay in a loop, probably wont end up being the best but can refine later

--- NumberGuessingGame/NumberGuessingGame/Program.cs
+++ NumberGuessingGame/NumberGuessingGame/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool cont = true;
+
+while (cont == true)
+{
+  Console.WriteLine("Do you wish to continue? (y/n)");
+  string? input = Console.ReadLine();
+
+  if (input == null)
+  {
+    Console.WriteLine("Please enter a valid answer");
+    continue;
+  }
+
+  input = input.Trim().ToLower();
+
+  if (input == "y" || input == "yes")
+  {
+    cont = true;
+  }
+  else if (input == "n" || input == "no")
+  {
+    cont = false;
+  }
+  else
+  {
+    Console.WriteLine("Please enter a valid answer");
+    continue;
+  }
+}
