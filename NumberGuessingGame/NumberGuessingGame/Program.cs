8d2d4f0db5b95bda71f77b3a5d56a6269420b403
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-08-12 15:49:32 +1200

    User can now choose that the number is chossen between 1 and whatever number they choose

--- NumberGuessingGame/NumberGuessingGame/Program.cs
+++ NumberGuessingGame/NumberGuessingGame/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
@@ -9,28 +10,47 @@ class Program
     while (cont == true)
     {
 
+      int maxNumber;
+
+      while (true)
+      {
+        Console.Write("Choose the highest number thats greater than 1: ");
+        string? maxNumberInput = Console.ReadLine();
+
+        if (maxNumberInput == null)
+        {
+          Console.WriteLine("Please enter a number");
+          continue;
+        }
+
+        if (int.TryParse(maxNumberInput, out maxNumber) && maxNumber > 1)
+          break;
+
+        Console.WriteLine("Please enter a valid number greater than 1");
+      }
+
       Random random = new();
 
-      int secretNumber = random.Next(1, 101);
+      int secretNumber = random.Next(1, maxNumber + 1);
 
       Console.WriteLine(secretNumber);
 
       Console.WriteLine("Do you wish to continue? (y/n)");
-      string? input = Console.ReadLine();
+      string? contInput = Console.ReadLine();
 
-      if (input == null)
+      if (contInput == null)
       {
         Console.WriteLine("Please enter a valid answer");
         continue;
       }
 
-      input = input.Trim().ToLower();
+      contInput = contInput.Trim().ToLower();
 
-      if (input == "y" || input == "yes")
+      if (contInput == "y" || contInput == "yes")
       {
         cont = true;
       }
-      else if (input == "n" || input == "no")
+      else if (contInput == "n" || contInput == "no")
       {
         cont = false;
       }
