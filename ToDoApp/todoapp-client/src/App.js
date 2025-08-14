f8d37f6a66c54c983edba4b0103798c629dea6be
Lane Pearce-Metcalfe <lpearcemetcalfe@gmail.com>
2025-08-14 12:34:50 +1200

    Added a basic react component with an input for a title and a button that can send a post request to the API

--- ToDoApp/todoapp-client/src/App.js
+++ ToDoApp/todoapp-client/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [title, setTitle] = useState('')
+
+  const addToDo = async () => {
+    try {
+      const res = await axios.post('https://localhost:5001/api/todo', { title })
+      console.log('Added todo:', res.data)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Add ToDo</h1>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Todo title"
+      />
+      <button onClick={addToDo}>Add</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
